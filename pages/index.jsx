e46cd7a73e113b8f7f90085fa1f532256e92c711
Wen Chenghui <wenchenghui@example.com>
2025-06-06 12:15:02 -0700

    fix: finally update homepage with nano

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,3 +7,12 @@ export default function Home() {
     </div>
   );
 }
+'use client';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-red-900 text-3xl font-bold">
+      Hello from Stanford AI Builder Club!
+    </div>
+  );
+}
